2845693cfd330092b1ca9090cfed37d2abbc1ce1
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-03-01 08:30:47 +0000

    Styled component Card created to be used throughout app for shared styling

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { useState } from 'react'
+import Card from './shared/Card'
 
 export default function FeedbackItem({ item }) {
 
 
   return (
-    <div className='card'>
+    <Card>
         <div className="num-display">{item.rating}</div>
         <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   )
 }
