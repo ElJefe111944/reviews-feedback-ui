b6431c0b589cef9de38b956bd571d258ec4043f8
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-03-03 22:18:07 +0000

    Delete comment functionality added

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -8,12 +8,20 @@ function App() {
 
     // app level state
     const [feedback, setFeedback] = useState(FeedbackData);
+    // delete feedback
+    const deleteFeedback = (id) => {
+        console.log('App', id);
+        // user confirms delete
+        if(window.confirm('Are you sure you want to delete?')){
+            setFeedback(feedback.filter((item) => item.id !== id ));
+        }        
+    }
 
     return (
         <>
         <Header />
         <div className="container">
-            <FeedbackList feedback={feedback} />    
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />    
         </div>
         </>
     )
--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -3,17 +3,13 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import Card from './shared/Card'
 
-export default function FeedbackItem({ item }) {
+export default function FeedbackItem({ item, handleDelete }) {
   
-  // close btn
-  const handleClick = (id) => {
-    console.log(id);
-  }
-
+ 
   return (
     <Card reverse={true}>
         <div className="num-display">{item.rating}</div>
-        <button onClick={() => handleClick(item.id)} className="close">
+        <button onClick={() => handleDelete(item.id)} className="close">
           <FaTimes color="purple" />
         </button>
         <div className="text-display">{item.text}</div>
