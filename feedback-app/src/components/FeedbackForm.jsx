a23724ba170fb2fe34260d9cf651cbb0c8e5a71a
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-03-06 08:51:04 +0000

    FeedbackForm component created and styling added

---
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState } from 'react'
+import Card from './shared/Card'
+
+function FeedbackForm() {
+
+    const [text, setText] = useState('');
+
+    const handleTextChange = (e) => {
+        setText(e.target.value);
+    };
+
+  return (
+    <Card>
+        <form action="">
+            <h2>How would you rate our services?</h2>
+            {/* rating selectors go here */}
+            <div className="input-group">
+                <input onChange={handleTextChange} type="text" value={text} name="review" id="review" placeholder='Write a review' />
+                <button type="submit">Send</button>
+            </div>
+        </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
