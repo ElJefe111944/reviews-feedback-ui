dd9347f0ea62253585c13c28a2af08a76f53080b
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-03-20 17:43:13 +0000

    Rating template and functionality added

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -2,22 +2,27 @@ import React from 'react'
 import { useState } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button';
+import RatingSelect from './RatingSelect';
 
 function FeedbackForm() {
 
     const [text, setText] = useState('');
+    const [rating, setRating] = useState(10);
     const [btnDisabled, setBtnDisabled] = useState(true);
     const [message, setMessage] = useState('');
 
     const handleTextChange = (e) => {
         // validation
         if(text === ''){
+
             setBtnDisabled(true);
             setMessage(null);
         } else if (text !== '' && text.trim().length <= 10){
+
             setBtnDisabled(true);
             setMessage('Review must be at least 10 characters')            
         } else {
+            
             setMessage(null);
             setBtnDisabled(false);
         }
@@ -31,6 +36,7 @@ function FeedbackForm() {
         <form action="">
             <h2>How would you rate our services?</h2>
             {/* rating selectors go here */}
+            <RatingSelect select={(rating) => { setRating(rating) }} />
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" value={text} name="review" id="review" placeholder='Write a review' />
                 <Button isDisabled={btnDisabled} type="submit">Send</Button>
