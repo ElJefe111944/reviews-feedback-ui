383fa0e47899d1a43e58dbd8507016f42987a5ae
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-03-22 16:23:05 +0000

    Error fixes

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+// import { motion, AnimatePresence } from 'framer-motion'
 import React from 'react'
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
@@ -10,6 +11,7 @@ export default function FeedbackList({ feedback, handleDelete }) {
         return <p>No Feedback Yet</p>
     }
 
+// version without animation
   return (
     <div className='feedback-list'>
         {feedback.map((item) => (
@@ -17,6 +19,28 @@ export default function FeedbackList({ feedback, handleDelete }) {
         ))}
     </div>
   )
+
+// return (
+//     <div className='feedback-list'>
+//             <AnimatePresence>
+//                 {
+//                     feedback.map((item) => (
+//                         <motion.div
+//                             key={item.id}
+//                             initial={{ opacity: 0 }}
+//                             animate={{ opacity: 1 }}
+//                             exit={{ opacity: 0 }}
+//                         >
+//                             <FeedbackItem
+//                                 key={item.id}
+//                                 item={item}
+//                                 handleDelete={handleDelete} />
+//                         </motion.div>
+//                     ))
+//                 }
+//             </AnimatePresence>
+//     </div>
+//   )
 }
 
 FeedbackList.prototypes = {
